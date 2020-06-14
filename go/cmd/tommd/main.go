d8e19c2a1c3b9de8807149f655b4d6ce5f74d193
Maxim <minimal.oasis@gmail.com>
2020-06-14 22:14:06 +0200

    first working version

--- go/cmd/tommd/main.go
+++ go/cmd/tommd/main.go
@@ -2,55 +2,85 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 
 	"mxmz.it/mxmz/tommaso/dto"
 	"mxmz.it/mxmz/tommaso/prober"
+	"mxmz.it/mxmz/tommaso/system"
 )
 
 func main() {
 
-	var specs = []*dto.ProbeSpec{
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 5,
-		},
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 5000,
-		},
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 5000,
-		},
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 5000,
-		},
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 5000,
-		},
-		{
-			Type:    "tcp",
-			Args:    []string{"172.27.203.2", "80"},
-			Timeout: 8,
-		},
+	var bseURL = "http://localhost:7997"
+	for {
+
+		var specs, err = getMyProbeSpecs(bseURL)
+		if err != nil {
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		var p = &prober.Prober{}
+		rv := p.RunProbSpecsConcurrent(specs)
+
+		//	var _ = err
+		var _ = rv
+		fmt.Printf("%v\n", jsonIndent(rv))
+		err = pushMyProbeResults(bseURL, rv)
+		fmt.Printf("%v\n", err)
+		time.Sleep(30 * time.Second)
 	}
 
-	var p = &prober.Prober{}
-	rv := p.RunProbSpecsConcurrent(specs)
-
-	//	var _ = err
-	var _ = rv
-	//fmt.Printf("%v\n", jsonIndent(rv))
 }
 
 func jsonIndent(v interface{}) string {
 	b, _ := json.MarshalIndent(v, " ", " ")
 	return string(b)
+}
+
+func getMyProbeSpecs(baseURL string) ([]*dto.ProbeSpec, error) {
+
+	var ifaces = system.GetNetInterfaceAddresses()
+	var body = dto.MySources{
+		Sources: ifaces,
+	}
+
+	var res, err = http.DefaultClient.Post(baseURL+"/api/agent/get-my-probe-specs", "application/json", strings.NewReader(jsonIndent(body)))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var probeSpecs []*dto.ProbeSpec
+	err = json.Unmarshal(resBody, &probeSpecs)
+	if err != nil {
+		return nil, err
+	}
+
+	return probeSpecs, nil
+
+}
+
+func pushMyProbeResults(baseURL string, results []*dto.ProbeResult) error {
+
+	var res, err = http.DefaultClient.Post(baseURL+"/api/agent/push-my-probe-results", "application/json", strings.NewReader(jsonIndent(results)))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	_, _ = ioutil.ReadAll(res.Body)
+	if res.StatusCode != http.StatusAccepted {
+		return errors.New(fmt.Sprintf("Unexpected status: %d", res.StatusCode))
+	}
+
+	return nil
+
 }
--- go/cmd/tommsrv/main.go
+++ go/cmd/tommsrv/main.go
@@ -1,21 +1,175 @@
 package main
 
-import "mxmz.it/mxmz/tommaso/dto"
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"mxmz.it/mxmz/tommaso/dto"
+	"mxmz.it/mxmz/tommaso/ports"
+	"mxmz.it/mxmz/tommaso/storage"
+)
+
+func init() {
+
+}
+
+type myContext struct {
+	echo.Context
+	probeSpecStore   ports.ProbeSpecStore
+	probeResultStore ports.ProbeResultStore
+}
+
+func setupContext(db *storage.FileDB) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			return next(&myContext{c, &storage.SimpleProbSpecStore{db}, &storage.VolatileProbResultStore{}})
+		}
+	}
+}
+
+func StoredProbeSpecFactory() interface{}     { return new(dto.StoredProbeSpec) }
+func StoredProbeSpecRuleFactory() interface{} { return new(dto.StoredProbeSpecRule) }
 
 func main() {
+
+	var db = storage.DefaultFileDB()
+
 	e := echo.New()
-	api := e.Group("/api")
-	api.POST("/my-probes", myProbes)
+	agentAPI := e.Group("/api/agent")
+	agentAPI.Use(setupContext(db))
+	agentAPI.POST("/get-my-probe-specs", getMyProbeSpecs)
+	agentAPI.POST("/push-my-probe-results", pushMyProbeResults)
+	dashboardAPI := e.Group("/api/dashboard")
+	dashboardAPI.Use(setupContext(db))
+	dashboardAPI.GET("/probe/results", getAllProbeResults)
+	dashboardAPI.GET("/probe/specs", listProbeSpecs)
+	dashboardAPI.PUT("/probe/specs/:id", putProbeSpec)
+	dashboardAPI.DELETE("/probe/specs/:id", deleteProbeSpec)
+	dashboardAPI.GET("/probe/rules", listProbeSpecRules)
+	dashboardAPI.PUT("/probe/rules/:id", putProbeSpecRule)
+	dashboardAPI.DELETE("/probe/rules/:id", deleteProbeSpecRule)
+
 	e.Logger.Fatal(e.Start(":7997"))
 }
 
-func myProbes(c echo.Context) error {
+func getMyProbeSpecs(c echo.Context) error {
+	var ctx = c.(*myContext)
 	var err error
-	var myNames dto.MyNames
-	if err = c.Bind(&myNames); err != nil {
+	var mySrcs dto.MySources
+	if err = c.Bind(&mySrcs); err != nil {
 		return err
 	}
-	c.JSON(200, []string{"boh"})
+	probSpecs, err := ctx.probeSpecStore.GetProbeSpecsForNames(ctx.Context.Request().Context(), &mySrcs)
+	if err != nil {
+		return err
+	}
+	c.JSON(200, probSpecs)
+	return nil
+}
+func pushMyProbeResults(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var err error
+	var myResults []*dto.ProbeResult
+	if err = c.Bind(&myResults); err != nil {
+		return err
+	}
+	err = ctx.probeResultStore.PutResultsForSources(ctx.Context.Request().Context(), myResults)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusAccepted, nil)
+	return nil
+}
+
+func getAllProbeResults(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var err error
+	var myResults []*dto.ProbeResult
+	if err = c.Bind(&myResults); err != nil {
+		return err
+	}
+	res, err := ctx.probeResultStore.GetResultsBySourcePrefix(ctx.Context.Request().Context(), "")
+	if err != nil {
+		return err
+	}
+	c.JSON(200, res)
+	return nil
+}
+
+func listProbeSpecs(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var err error
+	probSpecs, err := ctx.probeSpecStore.GetStoredProbeSpecs(ctx.Context.Request().Context())
+	if err != nil {
+		return err
+	}
+	c.JSON(200, probSpecs)
+	return nil
+}
+
+func putProbeSpec(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var id = c.Param("id")
+	var err error
+	var spec dto.ProbeSpec
+	if err = c.Bind(&spec); err != nil {
+		return err
+	}
+
+	err = ctx.probeSpecStore.PutStoredProbeSpec(ctx.Context.Request().Context(), id, &spec)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusNoContent, nil)
+	return nil
+}
+func deleteProbeSpec(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var id = c.Param("id")
+	var err error
+	err = ctx.probeSpecStore.PutStoredProbeSpec(ctx.Context.Request().Context(), id, nil)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusNoContent, nil)
+	return nil
+}
+
+func listProbeSpecRules(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var err error
+	probSpecRules, err := ctx.probeSpecStore.GetStoredProbeSpecRules(ctx.Context.Request().Context())
+	if err != nil {
+		return err
+	}
+	c.JSON(200, probSpecRules)
+	return nil
+}
+
+func putProbeSpecRule(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var id = c.Param("id")
+	var err error
+	var rule dto.ProbeSpecRule
+	if err = c.Bind(&rule); err != nil {
+		return err
+	}
+
+	err = ctx.probeSpecStore.PutStoredProbeSpecRule(ctx.Context.Request().Context(), id, &rule)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusNoContent, nil)
+	return nil
+}
+func deleteProbeSpecRule(c echo.Context) error {
+	var ctx = c.(*myContext)
+	var id = c.Param("id")
+	var err error
+	err = ctx.probeSpecStore.PutStoredProbeSpec(ctx.Context.Request().Context(), id, nil)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusNoContent, nil)
 	return nil
 }
