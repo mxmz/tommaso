552bcea648ff4a2283372fc57e31955ce971b8ff
Max Muzi <max.muzi@kqi.it>
2021-06-28 17:46:08 +0200

    "cnnection refused" is ok

--- go/prober/tcp.go
+++ go/prober/tcp.go
@@ -2,6 +2,7 @@ package prober
 
 import (
 	"net"
+	"strings"
 	"time"
 )
 
@@ -10,6 +11,10 @@ func TcpProbe(network string, timeout time.Duration) (time.Duration, error) {
 	conn, err := net.DialTimeout("tcp", network, timeout)
 	endTime := time.Now()
 	if err != nil {
+		if strings.Contains(err.Error(), "connect: connection refused") {
+			// connection refused means OK
+			return endTime.Sub(startTime), nil
+		}
 		return 0, err
 	} else {
 		defer conn.Close()
